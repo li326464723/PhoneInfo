d2dfc2494b502b1df0e15e657990a484ded7c4b1
lowry <lowry@21kunpeng.com>
2015-09-28 14:08:52 +0800

    test

--- src/test/DBTest.java
+++ src/test/DBTest.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBTest {
 	public static void main(String[] args) {
-		System.out.println("=============");
+		System.out.println("================");
 		String driverName = "com.mysql.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/phone_info?user=root&password=192526";
 		try {
