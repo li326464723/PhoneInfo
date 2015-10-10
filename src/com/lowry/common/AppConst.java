b709b69ebb1bb1b26386b9fa5341ae12b5a415fc
lowry <lowry@21kunpeng.com>
2015-10-10 16:04:29 +0800

    新增common包及const类

---
+++ src/com/lowry/common/AppConst.java
@@ -0,0 +1,46 @@
+package com.lowry.common;
+
+public class AppConst {
+	
+	/**********************厂商标识*******************************/
+	public static final String[] MOTOROLA = {"motorola","mot","FIH"};
+	public static final String[] SONYERICSSON = {"Sony Ericsson","Sony"};//索爱
+	public static final String[] SAMSUNG = {"samsung"};
+	public static final String[] HTC = {"HTC","HUN"};
+	public static final String[] LG = {"LGE"};
+	public static final String[] OPPO = {"alps","OPPO"};
+	public static final String[] TCL = {"TCL"};
+	public static final String[] LENOVO = {"LENOVO"};
+	public static final String[] ZTE = {"ZTE"};//中兴
+	public static final String[] MEIZU = {"Meizu"};//魅族
+	public static final String[] XIAOMI = {"Xiaomi"};//小米
+	public static final String[] HUAWEI = {"HUAWEI"};//华为
+	public static final String[] TIANYU = {"TIANYU","sprd","K-Touch"};//天语
+	public static final String[] COOLPAD = {"YuLong","CoolPad"};//酷派
+	public static final String[] AMAZON= {"Amazon"};//亚马逊
+	public static final String[] HISENSE= {"Hisense"};//海信
+	public static final String[] ACER= {"Acer"};//宏基
+	public static final String[] JINLI= {"GIONEE"};//金立
+	public static final String[] PHILIPS= {"Philips"};//飞利浦
+	public static final String[] GOOGLE= {"asus"};//谷歌
+	public static final String[] SHENGDA= {"snda.com"};//盛大
+	public static final String[] KUBI= {"koobee"};//酷比
+	public static final String[] XIAXIN= {"AMOI"};//夏新
+	public static final String[] FANSHANG= {"Fason"};//梵尚
+	public static final String[] AINUO= {"ainol"};//艾诺
+	public static final String[] DELL= {"Dell"};//戴尔
+	public static final String[] DPD= {"dlkjl12389"};//多普达
+	public static final String[] HAIER= {"haier"};//海尔
+	public static final String[] SHARP= {"sharp"};//夏普
+	public static final String[] BBK= {"BBK"};//步步高
+	public static final String[] NUBIA= {"nubia"};//努比亚
+	public static final String[] KONKA= {"KONKA"};//康佳
+	public static final String[] DOOV= {"DOOV"};//朵唯
+	public static final String[] CHINAMOBILE= {"CMDC"};//CHINAMOBILE
+	public static final String[] BEE= {"bee"};//小蜜蜂
+	public static final String[] OPSSON= {"OPSSON"};//欧博信
+	public static final String[] SMARTISAN= {"smartisan"};//锤子
+	public static final String[] IUSAI= {"IUSAI"};//优赛
+
+	
+}
